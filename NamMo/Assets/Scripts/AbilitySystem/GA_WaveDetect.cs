b353a637466636019a82d1f13dc4885ce6657629
kjhcocomi <981028_@naver.com>
2024-07-30 11:59:38 +0900

    애니메이터 정리

--- NamMo/Assets/Scripts/AbilitySystem/GA_WaveDetect.cs
+++ NamMo/Assets/Scripts/AbilitySystem/GA_WaveDetect.cs
@@ -42,7 +42,7 @@ public class GA_WaveDetect : GameAbility
     protected override bool CanActivateAbility()
     {
         if (base.CanActivateAbility() == false) return false;
-        //if (_asc.gameObject.GetComponent<PlayerMovement>().IsJumping || _asc.gameObject.GetComponent<PlayerMovement>().IsDashing) return false;
+        if (_asc.gameObject.GetComponent<PlayerMovement>().IsGround() == false) return false;
         return true;
     }
     protected override void EndAbility()
