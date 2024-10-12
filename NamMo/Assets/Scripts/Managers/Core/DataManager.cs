806992d384f78bbd731e163e1f576f7754695953
kjhcocomi <981028_@naver.com>
2024-10-12 17:07:14 +0900

    feature : 스테이지당 적 정보 데이터로 관리하기 테스트

--- NamMo/Assets/Scripts/Managers/Core/DataManager.cs
+++ NamMo/Assets/Scripts/Managers/Core/DataManager.cs
@@ -16,7 +16,10 @@ public class DataManager
     public Dictionary<int, Data.Conversation> TutorialSpeechBubbleDict { get; private set; } = new Dictionary<int, Conversation>();
     public Dictionary<int, Data.CharacterInfo> CharacterInfoDict { get; private set; } = new Dictionary<int, Data.CharacterInfo>();
     public Dictionary<Define.TutorialType, Data.TutorialInfo> TutorialInfoDict { get; private set; } = new Dictionary<Define.TutorialType, Data.TutorialInfo>();
+    public Dictionary<int, Data.Enemy> EnemyDict { get; private set; } = new Dictionary<int, Data.Enemy>();
+    public Dictionary<Define.Scene, Data.StageEnemy> StageEnemyDict { get; private set; } = new Dictionary<Define.Scene, Data.StageEnemy>();
     public PlayerData PlayerData;
+    public EnemyData EnemyData;
     public void Init()
     {
         PrologueFadeInScriptDict = LoadJson<Data.PrologueFadeInScriptData, int, Data.PrologueFadeInScript>("PrologueFadeInScriptData").MakeDict();
@@ -26,8 +29,11 @@ public class DataManager
 
         CharacterInfoDict = LoadJson<Data.CharacterInfoData, int, Data.CharacterInfo>("CharacterInfoData").MakeDict();
         TutorialInfoDict = LoadJson<Data.TutorialInfoData, Define.TutorialType, Data.TutorialInfo>("TutorialInfoData").MakeDict();
+        EnemyDict = LoadJson<Data.EnemyData, int, Data.Enemy>("EnemyData").MakeDict();
+        StageEnemyDict = LoadJson<Data.StageEnemyData, Define.Scene, Data.StageEnemy>("StageEnemyData").MakeDict();
 
         PlayerData = GameData.Load<PlayerData>();
+        EnemyData = GameData.Load<EnemyData>();
     }
 
     Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
@@ -38,9 +44,11 @@ public class DataManager
     public void SaveAllData()
     {
         PlayerData.Save();
+        EnemyData.Save();
     }
     public void ClearAllData()
     {
         PlayerData.Clear();
+        EnemyData.Clear();
     }
 }
