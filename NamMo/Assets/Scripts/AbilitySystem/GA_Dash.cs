0bbdd064a143001fdba5dfcb127e029ad38c7d17
kjhcocomi <981028_@naver.com>
2024-11-08 16:59:03 +0900

    feature : 남모 조작 수정

--- NamMo/Assets/Scripts/AbilitySystem/GA_Dash.cs
+++ NamMo/Assets/Scripts/AbilitySystem/GA_Dash.cs
@@ -7,7 +7,10 @@ public class GA_Dash : GameAbility
 {
     [SerializeField] private float _dashForce;
     [SerializeField] private float _dashTime;
+    [SerializeField] private float _invincibleTime;
 
+    private Coroutine _invincibleCoroutine = null;
+    private int _originExcludeLayer;
     protected override void Init()
     {
         _asc.gameObject.GetComponent<PlayerMovement>().OnDashEnd += EndAbility;
@@ -18,10 +21,34 @@ public class GA_Dash : GameAbility
         //Managers.Sound.Play("Dash");
         _asc.GetPlayerController().GetPlayerSound().PlayDashSound();
         _asc.gameObject.GetComponent<PlayerMovement>().Dash(_dashForce, _dashTime, Define.DashType.DefaultDash);
+
+        _invincibleCoroutine = StartCoroutine(CoApplyInvincible());
+
+        _originExcludeLayer = _asc.GetPlayerController().GetComponent<Collider2D>().excludeLayers;
+        _asc.GetPlayerController().GetComponent<Collider2D>().excludeLayers = LayerMask.GetMask("Enemy");
     }
     protected override void CancelAbility()
     {
         base.CancelAbility();
         _asc.gameObject.GetComponent<PlayerMovement>().CancelDash(Define.DashType.DefaultDash);
+        //EndAbility();
+    }
+    protected override void EndAbility()
+    {
+        base.EndAbility();
+        if (_invincibleCoroutine != null)
+        {
+            StopCoroutine(_invincibleCoroutine);
+            _asc.RemoveTag(Define.GameplayTag.Player_State_Invincible);
+            _invincibleCoroutine = null;
+        }
+        _asc.GetPlayerController().GetComponent<Collider2D>().excludeLayers = _originExcludeLayer;
+    }
+    IEnumerator CoApplyInvincible()
+    {
+        _asc.AddTag(Define.GameplayTag.Player_State_Invincible);
+        yield return new WaitForSeconds(_invincibleTime);
+        _asc.RemoveTag(Define.GameplayTag.Player_State_Invincible);
+        _invincibleCoroutine = null;
     }
 }
